b8d1724027a5635d9eaa4a24622a227bdbd6cd3b
suinua <34801662+suinua@users.noreply.github.com>
2022-03-08 17:31:43 +0900

    reset token

--- bin/code_viwer.dart
+++ bin/code_viwer.dart
@@ -5,7 +5,7 @@ import 'package:nyxx/nyxx.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  final bot = NyxxFactory.createNyxxWebsocket('ODczMzY4MjQ0NDcwMzAwNzEy.YQ3Zjw.MSzcxLgW8JnoauY3AmAGThgv0To',
+  final bot = NyxxFactory.createNyxxWebsocket('Token',
       GatewayIntents.allUnprivileged)
     ..registerPlugin(Logging())
     ..registerPlugin(CliIntegration())
